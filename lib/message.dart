868e2eb63a4a7f25961a915c7dc1dbaa06dee0b1
Vedant <harshtewari6@gmail.com>
2021-03-27 00:36:53 +0530

    Message Started

---
+++ lib/message.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:main/creds_database.dart';
+
+class MessagePage extends StatefulWidget {
+  @override
+  _MessagePageState createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: databaseReference.child("messages/").once(),
+        // ignore: missing_return
+        builder: (BuildContext context, AsyncSnapshot _snapshot) {});
+  }
+}
